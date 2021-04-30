3d9be75da862d2d3e3b2410272ce8bc22dfacbeb
Mary Cohan <mary.cohan@gmail.com>
2021-04-29 17:04:24 -0700

    deploying

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/homework", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
